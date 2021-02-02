22a256dd45ab15e2bee826db931cfc3914f45caa
Kitsunetic <shenhaichenhai@gmail.com>
2021-02-02 22:37:36 +0900

    weighted cross entropy

--- main.py
+++ main.py
@@ -7,10 +7,13 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch_optimizer
+from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.model_selection import StratifiedKFold
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 from torch.utils.data import DataLoader, Dataset, TensorDataset
 from torch.utils.tensorboard import SummaryWriter
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 import networks
 from utils import AccuracyMeter, AverageMeter, generate_experiment_directory
@@ -18,13 +21,80 @@ from utils import AccuracyMeter, AverageMeter, generate_experiment_directory
 LOGDIR = Path("log")
 RESULT_DIR = Path("results")
 DATA_PATH = Path("data/0201.npz")
-COMMENT = "resnet150-aug_shift80_pad0"
+COMMENT = "resnet34-aug_shift50_5_pad0"
 
 EXPATH, EXNAME = generate_experiment_directory(RESULT_DIR, COMMENT)
 
 BATCH_SIZE = 256
 NUM_CPUS = 8
 EPOCHS = 200
+
+CE_WEIGHTS = torch.tensor(
+    [
+        4.27,
+        2.44,
+        2.56,
+        2.23,
+        1.46,
+        2.05,
+        2.13,
+        1.97,
+        0.53,
+        1.38,
+        2.56,
+        2.23,
+        4.27,
+        4.27,
+        2.05,
+        2.05,
+        2.33,
+        1.90,
+        1.09,
+        2.56,
+        1.97,
+        1.90,
+        2.70,
+        2.56,
+        1.46,
+        2.13,
+        0.03,
+        1.51,
+        0.93,
+        2.56,
+        1.46,
+        2.56,
+        2.85,
+        2.56,
+        2.33,
+        1.71,
+        1.83,
+        1.46,
+        2.56,
+        2.56,
+        1.51,
+        2.56,
+        2.56,
+        1.46,
+        2.44,
+        2.33,
+        2.56,
+        1.97,
+        2.05,
+        1.71,
+        1.38,
+        2.13,
+        4.27,
+        3.94,
+        2.23,
+        1.38,
+        1.42,
+        2.56,
+        2.56,
+        2.23,
+        1.07,
+    ],
+    dtype=torch.float32,
+)
 
 
 class QDataset(TensorDataset):
@@ -49,15 +119,15 @@ class QDataset(TensorDataset):
     @staticmethod
     def _aug_random_shift(x):
         """
-        80% 확률로 랜덤하게 왼쪽/오른쪽으로 shift한다.
+        50% 확률로 랜덤하게 왼쪽/오른쪽으로 shift한다.
         잘리는 부분은 버리고, 새로운 부분은 0으로 채움.
 
         TODO 잘 안됨
         """
-        if random.random() >= 80:
+        if random.random() >= 50:
             return x
 
-        dist = round(torch.randn(1).item() * 20)
+        dist = round(torch.randn(1).item() * 5)
         x = torch.roll(x, dist, dims=1)
         if dist > 0:
             x[:, :dist] = 0
@@ -95,7 +165,7 @@ class Trainer:
 
     def fit(self, dl_train, dl_valid, num_epochs):
         self.num_epochs = num_epochs
-        self.scheduler = ReduceLROnPlateau(self.optimizer, factor=0.25, patience=3, verbose=True, threshold=1e-8, cooldown=2)
+        self.scheduler = ReduceLROnPlateau(self.optimizer, factor=0.25, patience=5, verbose=True, threshold=1e-8, cooldown=3)
         self.best_loss = math.inf
         self.earlystop_cnt = 0
         self.earlystop = False
@@ -106,6 +176,7 @@ class Trainer:
             self.callback(epoch)
 
             if self.earlystop:
+                self.report(dl_valid)
                 break
 
     def train_loop(self, dl):
@@ -163,9 +234,57 @@ class Trainer:
         else:
             self.earlystop_cnt += 1
 
-        if self.earlystop_cnt > 10:
+        if self.earlystop_cnt > 20:
             print("[Early Stop] Stop training")
             self.earlystop = True
+
+    def report(self, dl):
+        # 가장 좋은 checkpoint 로딩
+        ckpt = torch.load(self.expath / "best-ckpt.pth")
+        self.model.load_state_dict(ckpt)
+        self.model.eval()
+
+        ys, ps = [], []
+        self.val_loss, self.val_acc = AverageMeter(), AccuracyMeter()
+        with torch.no_grad():
+            for x, y in dl:
+                y_ = y.cuda()
+                p = self.model(x.cuda())
+                loss = self.criterion(p, y_)
+
+                self.val_loss.update(loss.item())
+                self.val_acc.update(y_, p)
+                ys.append(y)
+                ps.append(p.cpu())
+
+        Y = torch.cat(ys).numpy()
+        P = torch.cat(ps)
+        P = torch.argmax(P, dim=1).numpy()
+
+        # DEBUG: Y, P txt파일로 저장
+        with open(self.expath / "YP.txt", "w") as f:
+            for y, p in zip(Y, P):
+                f.write(f"{y} {p}\r\n")
+
+        # loss, acc
+        self.writer.add_text(
+            self.exname + "/best",
+            f"loss: {self.val_loss():.6f}, acc: {self.val_acc()*100:.2f}%",
+        )
+
+        # classification report
+        cr = classification_report(Y, P, labels=list(range(61)))
+        cr = cr.replace("\n", "<br>").replace(" ", "&nbsp;")
+        self.writer.add_text(self.exname + "/classification_report", cr)
+
+        # confusion matrix
+        cm = confusion_matrix(Y, P, labels=list(range(61)))
+        fig = plt.figure(figsize=(24, 20))
+        sns.heatmap(cm, annot=True, fmt="g", cmap="Blues", cbar=False)
+        plt.xlabel("pred")
+        plt.ylabel("real")
+        plt.title(EXNAME)
+        self.writer.add_figure(self.exname + "/confusion_matrix", fig)
 
     def evaluate(self, dl):
         self.model.eval()
@@ -197,8 +316,8 @@ def main():
         dl_train = DataLoader(ds_train, **dl_kwargs, shuffle=True)
         dl_valid = DataLoader(ds_valid, **dl_kwargs, shuffle=False)
 
-        model = networks.ResNet152().cuda()
-        criterion = nn.CrossEntropyLoss().cuda()
+        model = networks.ResNet34().cuda()
+        criterion = nn.CrossEntropyLoss(weight=CE_WEIGHTS).cuda()
         optimizer = torch_optimizer.RAdam(model.parameters(), lr=1e-4)
 
         trainer = Trainer(model, criterion, optimizer, writer, EXNAME, EXPATH, fold)
